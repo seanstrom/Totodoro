c43e3796f54c61c879e10dc9ff8b1392c2a541b7
Sean Hagstrom <sean@seanhagstrom.com>
2022-10-15 11:08:56 +0100

    Initial Commit

---
+++ Totodoro-Timer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Totodoro-Timer
+//
+//  Created by Sean Hagstrom on 10/15/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
